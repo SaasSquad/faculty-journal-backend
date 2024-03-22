07c717f5de6175d27898cf10f69cba11614d2edf
Defdave <igedavid01@gmail.com>
2024-03-22 04:19:49 +0100

    send the jwt token to the client

--- src/routes/userlogin.js
+++ src/routes/userlogin.js
@@ -4,25 +4,24 @@ const User = require("../schema/signupSchema");
 const { comparePassword } = require("../middleware/hashPassword");
 const router = Router();
 
-router.post("/login", async (req, res) => {
+router.post("/login", async(req, res) => {
     const { email, password } = req.body;
-    if(!email || !password) {return res.sendStatus(400)};
+    if (!email || !password) { return res.sendStatus(400) };
     //find user in database
     const userDB = await User.findOne({ email });
-    if(!userDB) {return res.sendStatus(401)};
+    if (!userDB) { return res.sendStatus(401) };
     //compare password
     const isValid = comparePassword(password, userDB.password);
-    if(!isValid){
+    if (!isValid) {
         return res.sendStatus(401);
     }
     //generate Jwt token
-    const token = jwt.sign({ userId: userDB._id }, process.env.JWT_SECRET_KEY, { expiresIn: "48h"});
-    res.cookie("jwt", token, 
-    { httpOnly : false });
+    const token = jwt.sign({ userId: userDB._id }, process.env.JWT_SECRET_KEY, { expiresIn: "48h" });
+    res.cookie("jwt", token, { httpOnly: false });
 
     //send success respond
 
-    return res.sendStatus(200);
+    return res.status(200).json(token);
 })
 
 module.exports = router;
