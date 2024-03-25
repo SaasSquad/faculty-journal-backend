ffb07172ec2bc71b0e5e353bddf3c5075cde455e
Defdave <igedavid01@gmail.com>
2024-03-25 12:56:30 +0100

    fixing route pdf

--- src/routes/getFile.js
+++ src/routes/getFile.js
@@ -1,24 +1,26 @@
 const express = require("express");
 const { Router } = require("express");
 const router = Router();
-const routeAuthn= require("../middleware/jwtAuth");
+const routeAuthn = require("../middleware/jwtAuth");
 const Articles = require("../schema/Article")
+const path = require("path")
 
 router.get("/file/:id", async(req, res) => {
     const { id } = req.params;
-    try{
+    try {
         const article = await Articles.findById(id);
-        const file = article.file;
+        const rootDir = path.resolve(__dirname, '../../Public/Images/')
+        const file = path.join(rootDir, article.file)
 
-        if(!file) {
+        if (!file) {
             res.status(404).json({ error: "file not found" });
-        } else{
+        } else {
             res.setHeader("content-Type", "application/pdf");
             res.setHeader("content-Disposition", `attachment: filename="${article.title}.pdf"`)
-            res.send(file);
+            res.sendFile(file);
         }
-    } catch(error) {
-        req.status(500);
+    } catch (error) {
+        res.status(500);
     }
 });
 
